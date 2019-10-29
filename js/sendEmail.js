5dcf8665b6bd4fc797728c1dfddd6e10ac3f9221
gsak3l <gsakel5@gmail.com>
2019-10-29 21:55:54 +0200

    email doesnt really want to work properly for some reason

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,18 +1,10 @@
 function sendMail(contactForm)
 {
+    console.log("arxi");
     emailjs.send("gmail", "newdefaulttemplate", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.Email.value,
         "Subject": contactForm.Message.value
     })
-    .then(
-        function(response)
-        {
-            console.log("success", response);
-        }, 
-        function(erorr)
-        {
-            console.log("FAIL");
-        }
-    )
+    console.log("telos");
 }
