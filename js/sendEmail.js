ec814a1e1b4a5b396ccff84997b336b4871ba379
gsak3l <gsakel5@gmail.com>
2019-10-29 10:43:22 +0200

    nothing is working

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,1 +1,18 @@
-console.log("hello world!");
+function sendMail(contactForm)
+{
+    emailjs.send("gmail", "newdefaulttemplate", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.Email.value,
+        "Subject": contactForm.Message.value
+    })
+    .then(
+        function(response)
+        {
+            console.log("success", response);
+        }, 
+        function(erorr)
+        {
+            console.log("FAIL");
+        }
+    )
+}
